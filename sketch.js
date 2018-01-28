6a72c3af9d1cf43bdcf89f632fa7a1c524d281e2
Emily Po <ewp2111@columbia.edu>
2018-01-28 10:17:19 -0500

    Clock

--- sketch.js
+++ sketch.js
@@ -6,20 +6,35 @@ function setup() {
 
 function draw() {
   background(100);
-  stroke(216, 163, 163);
-  strokeWeight(1);
-  line(50, 0, 50, 1000);
   noStroke();
-  textSize(20);
+  textSize(100);
   fill(255);
   textFont('Futura PT');
-  text('Emily Po', 150, 100);
+  text('mily Po', 300, 190);
   fill(255);
   textSize(10);
   textFont('Futura PT');
   text('DataViz SP18', 900, 50);
-  rect(70, 50, 50, 50);
-  translate(10 ,10);
-  rect(70, 50, 50, 50);
+  push();
+  var m = minute();
+  translate(m, m);
+  stroke(216, 163, 163);
+  strokeWeight(1);
+  line(50, 0, 50, 1000);
+  pop();
+  fill(255);
+  rect(250, 180, 40, 8);
+  fill(255);
+  rect(250, 150, 40, 8);
+  fill(255);
+  rect(250, 120, 40, 8);
+  push();
+  scale(hour(), hour());
+  rect(0, 0, 5, 5);
+  pop();
+  translate(250,120);
+  angleMode(DEGREES);
+  rotate(map(second(), 0, 60, 0, 90));
+  fill(255);
+  rect(0, 0, 8, 60);
 }
-
