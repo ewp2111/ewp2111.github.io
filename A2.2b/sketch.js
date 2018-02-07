093b3a6d048de87aa930317babc30cdf3a5baa07
Emily Po <ewp2111@columbia.edu>
2018-02-07 01:00:43 -0500

    new

--- A2.2b/sketch.js
+++ A2.2b/sketch.js
@@ -1,28 +1,48 @@
-var img;
 
 function preload() {
   sun = loadImage('sun.png');
   i1= loadImage('i1.png');
   i2= loadImage('i2.png');
   i3= loadImage('i3.png');
-  l1= loadImage('l1.png');
-  l2= loadImage('l2.png');
-  l3= loadImage('l3.png');
 
 }
 
 function setup() {
   createCanvas(5000, 5000);
-  frameRate(1);
+  frameRate(100);
 }
 
 function draw() {
- 	var t = map(second(),0,60,0,255);
-	tint(255,t);
-	image(i1,10,200,200,200);
+  var s = second();
+  var s1 = map(second(),0,60,0,800);
+  noStroke();
+  fill(0);
+  ellipse(s1+100,100,200,200);
 
 
+
+  var t1 = map(s,0,30,0,255);
+  var t2 = map(s,30,60,0,255);
+  if (s>30){
+  fill(255);
+  noStroke();
+  rect(10,200,200,200);
+  tint (255,t2);
+  image(i3,10,200,200,200);
+  tint (255,255-t2);
+  image (i2,10,200,200,200);
+  } else {
+  fill(255);
+  noStroke();
+  rect(10,200,200,200);
+  tint(255,255-t1);
+  image(i1,10,200,200,200);
+  tint(255,t1);
+  image(i2,10,200,200,200)
+  }
 }
 
- console.log(t)
+
+
+
 
