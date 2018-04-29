a9b2f8274b4d5e27ca9827c1610a057807f96496
Emily Po <ewp2111@columbia.edu>
2018-04-29 19:30:15 -0400

    7.1

--- A7.1/sketch.js
+++ A7.1/sketch.js
@@ -1,6 +1,5 @@
 var state = 0;
 
-
 function preload() {
   Aller_Lt = loadFont('Aller_Lt.ttf');
   Aller_Rg = loadFont('Aller_Rg.ttf');
@@ -12,16 +11,18 @@ function preload() {
   b5= loadImage('beer logos-05.png');
   b6= loadImage('beer logos-06.png');
   b7= loadImage('beer logos-07.png');
-  table = loadTable('breweryno.csv', 'csv', 'header');
+  table = loadTable('brewerynon.csv', 'csv', 'header');
 }
 
 function setup() {
-  createCanvas(1920, 1080);
+  createCanvas(1920, 3000);
   background (150);
   frameRate(30);
+  noLoop();
 }
 
 function draw() {
+
   fill(0);
   textFont(Aller_Bd);
   textSize(30);
@@ -88,6 +89,9 @@ function draw() {
     rect(40,160,350,400);
     fill(100);
     rect(40,460,350,40);
+  } else {
+    fill(150);
+    rect(40,160,350,400);
   };
   //text and image//
   fill(255);
@@ -202,10 +206,195 @@ function draw() {
     textSize(30);
     text(fruit, 700,160,500,300);
     image(b7,510,160,200,200)
-  }
+  };
+
+  //ingredients graph//
+  //legend (S,M,L,XL)//
+  fill(255);
+  textFont(Aller_Bd);
+  textSize(15);
+  text('Type of Ingredient',90,1120);
+  textFont(Aller_Lt);
+  text('Malt\nRice\nDry Hops\nHop Extract',90,1150);
+  textSize(20);
+  var noS = table.getColumn("noS");
+  var noM = table.getColumn("noM");
+  var noL = table.getColumn("noL");
+  var noXL = table.getColumn("noXL");
+  var tbS = table.getColumn("tbS");
+  var tbM = table.getColumn("tbM");
+  var tbL = table.getColumn("tbL");
+  var tbXL = table.getColumn("tbXL");
+  var year = table.getColumn("year");
+
+  // console.log(year);
+  //title
+  textSize(30);
+  textFont(Aller_Bd);
+  textAlign(CENTER);
+  text('Ingredient Consumption by year',1100,550);
+  //axis
+  textSize(20);
+  stroke(255);
+  strokeWeight(1);
+  //yaxis
+  line(500,600,500,1200);
+  //xaxis
+  line(500,1200,1700,1200);
+  push();
+  translate(400,900);
+  angleMode(DEGREES);
+  rotate(-90);
+  noStroke();
+  text('Pounds',0,0);
+  pop();
+  noStroke();
+  text('Year',1100,1300);
+  var Yaxis= [0,1000,2000,3000,4000,5000,6000];
+  textFont(Aller_Lt);
+  for (var i =0; i<Yaxis.length; i++) {
+    text(Yaxis[i],440,1200-600/7*i)
+  };
+  for (var i =0; i<year.length; i++) {
+    text(year[i],(500+1200/12*(i+1)),1230)
+  };
+  //plot S//
+
+  push();
+  translate(0,900);
+  //no of breweries graph//
+  //legend (S,M,L,XL)//
+  fill(255);
+  textFont(Aller_Bd);
+  textAlign(LEFT);
+  textSize(15);
+  text('Size of Breweries',90,1120);
+  textFont(Aller_Lt);
+  text('XL: >6M Barrels\nL: 2M - 6M Barrels\nM: 60,000-2M Barrels\nS: <60,000 Barrels',90,1150);
+  textSize(20);
+  var noS = table.getColumn("noS");
+  var noM = table.getColumn("noM");
+  var noL = table.getColumn("noL");
+  var noXL = table.getColumn("noXL");
+  var tbS = table.getColumn("tbS");
+  var tbM = table.getColumn("tbM");
+  var tbL = table.getColumn("tbL");
+  var tbXL = table.getColumn("tbXL");
+  var year = table.getColumn("year");
+
+  // console.log(year);
+  //title
+  textSize(30);
+  textFont(Aller_Bd);
+  textAlign(CENTER);
+  text('No. of Breweries by year',1100,550);
+  //axis
+  textSize(20);
+  stroke(255);
+  strokeWeight(1);
+  //yaxis
+  line(500,600,500,1200);
+  //xaxis
+  line(500,1200,1700,1200);
+  push();
+  translate(400,900);
+  angleMode(DEGREES);
+  rotate(-90);
+  noStroke();
+  text('No. of Breweries',0,0);
+  pop();
+  noStroke();
+  textAlign(CENTER);
+  text('Year',1100,1300);
+  var Yaxis= [0,1000,2000,3000,4000,5000,6000];
+  textFont(Aller_Lt);
+  for (var i =0; i<Yaxis.length; i++) {
+    text(Yaxis[i],440,1200-600/7*i)
+  };
+  for (var i =0; i<year.length; i++) {
+    text(year[i],(500+1200/12*(i+1)),1230)
+  };
+  //plot S//
+  stroke(255);
+  fill(255);
+  strokeWeight(2);
+  var S0= 1800-map(noS[0],0,7000,600,1200);
+  var S1= 1800-map(noS[1],0,7000,600,1200);
+  var S2= 1800-map(noS[2],0,7000,600,1200);
+  var S3= 1800-map(noS[3],0,7000,600,1200);
+  var S4= 1800-map(noS[4],0,7000,600,1200);
+  var S5= 1800-map(noS[5],0,7000,600,1200);
+  var S6= 1800-map(noS[6],0,7000,600,1200);
+  var S7= 1800-map(noS[7],0,7000,600,1200);
+  var S8= 1800-map(noS[8],0,7000,600,1200);
+  var S9= 1800-map(noS[9],0,7000,600,1200);
+  var S10= 1800-map(noS[10],0,7000,600,1200);
+  var S = [S0,S1,S2,S3,S4,S5,S6,S7,S8,S9,S10];
+  for (var i =0;i< S.length; i++) {
+    line(500+1200/12*(i+1),S[i],500+1200/12*(i+2),S[i+1]);
+    ellipse(500+1200/12*(i+1),S[i],10,10);
+  };
+
+  //plot M//
+  stroke(255,0,0);
+  fill(255,0,0);
+  var M0= 1800-map(noM[0],0,7000,600,1200);
+  var M1= 1800-map(noM[1],0,7000,600,1200);
+  var M2= 1800-map(noM[2],0,7000,600,1200);
+  var M3= 1800-map(noM[3],0,7000,600,1200);
+  var M4= 1800-map(noM[4],0,7000,600,1200);
+  var M5= 1800-map(noM[5],0,7000,600,1200);
+  var M6= 1800-map(noM[6],0,7000,600,1200);
+  var M7= 1800-map(noM[7],0,7000,600,1200);
+  var M8= 1800-map(noM[8],0,7000,600,1200);
+  var M9= 1800-map(noM[9],0,7000,600,1200);
+  var M10= 1800-map(noM[10],0,7000,600,1200);
+  var M = [M0,M1,M2,M3,M4,M5,M6,M7,M8,M9,M10];
+  for (var i =0;i< M.length; i++) {
+    line(500+1200/12*(i+1),M[i],500+1200/12*(i+2),M[i+1]);
+    ellipse(500+1200/12*(i+1),M[i],10,10);
+  };
+  //plot L//
+    stroke(0,255,0);
+    fill(0,255,0);
+  var L0= 1800-map(noL[0],0,7000,600,1200);
+  var L1= 1800-map(noL[1],0,7000,600,1200);
+  var L2= 1800-map(noL[2],0,7000,600,1200);
+  var L3= 1800-map(noL[3],0,7000,600,1200);
+  var L4= 1800-map(noL[4],0,7000,600,1200);
+  var L5= 1800-map(noL[5],0,7000,600,1200);
+  var L6= 1800-map(noL[6],0,7000,600,1200);
+  var L7= 1800-map(noL[7],0,7000,600,1200);
+  var L8= 1800-map(noL[8],0,7000,600,1200);
+  var L9= 1800-map(noL[9],0,7000,600,1200);
+  var L10= 1800-map(noL[10],0,7000,600,1200);
+  var L = [L0,L1,L2,L3,L4,L5,L6,L7,L8,L9,L10];
+  for (var i =0;i< L.length; i++) {
+    line(500+1200/12*(i+1),L[i],500+1200/12*(i+2),L[i+1]);
+    ellipse(500+1200/12*(i+1),L[i],10,10);
+  };
+  //plot XL//
+  stroke(0,0,255);
+  fill(0,0,255);
+  var XL0= 1800-map(noXL[0],0,7000,600,1200);
+  var XL1= 1800-map(noXL[1],0,7000,600,1200);
+  var XL2= 1800-map(noXL[2],0,7000,600,1200);
+  var XL3= 1800-map(noXL[3],0,7000,600,1200);
+  var XL4= 1800-map(noXL[4],0,7000,600,1200);
+  var XL5= 1800-map(noXL[5],0,7000,600,1200);
+  var XL6= 1800-map(noXL[6],0,7000,600,1200);
+  var XL7= 1800-map(noXL[7],0,7000,600,1200);
+  var XL8= 1800-map(noXL[8],0,7000,600,1200);
+  var XL9= 1800-map(noXL[9],0,7000,600,1200);
+  var XL10= 1800-map(noXL[10],0,7000,600,1200);
+  var XL = [XL0,XL1,XL2,XL3,XL4,XL5,XL6,XL7,XL8,XL9,XL10];
+  for (var i =0;i< L.length; i++) {
+    line(500+1200/12*(i+1),XL[i],500+1200/12*(i+2),L[i+1]);
+    ellipse(500+1200/12*(i+1),XL[i],10,10);
+  };
+  pop();
+
 }
-
-
 
 function mousePressed() {
   if (mouseX >40
@@ -260,14 +449,6 @@ function mousePressed() {
 //hover over display detail quantity//
 //hover over for trend switching to dry hops//
 
-
-
-//no of breweries graph//
-//legend (S,M,L,XL)//
-//plot S//
-//plot M//
-//plot L//
-//plot XL//
 
 
 //tax rate//
